d6312a6ce17b010cf54a154e598a149e5d6e0434
Zonirina Mick Ramahazomanana <zramahaz@c1r4s8.42antananarivo.mg>
2024-07-11 14:30:26 +0300

    minitalk

--- client_bonus.c
+++ client_bonus.c
@@ -6,33 +6,11 @@
 /*   By: zramahaz <zramahaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 14:12:49 by zramahaz          #+#    #+#             */
-/*   Updated: 2024/06/29 15:38:06 by zramahaz         ###   ########.fr       */
+/*   Updated: 2024/07/11 14:24:43 by zramahaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-static int	ft_atoi(const char *str);
-static void ft_config_signals(void);
-static void sig_handler(int signum);
-static void ft_send_msg(pid_t pid_server, char *msg);
-
-int main(int argc, char **argv)
-{
-    pid_t   pid_server;
-
-    if (argc != 3 || argv[2][0] == '\0')
-    {
-        ft_printf("Error\n");
-		return (1);
-    }
-	pid_server = ft_atoi(argv[1]);
-    ft_config_signals();
-    ft_send_msg(pid_server, argv[2]);
-    while (1)
-        pause();
-    return (0);
-}
 
 static int	ft_atoi(const char *str)
 {
@@ -61,59 +39,78 @@ static int	ft_atoi(const char *str)
 	return (resultat);
 }
 
-static void ft_config_signals(void)
+void	args_check(int argc, char **argv)
 {
-    struct sigaction    sa;
-    
-    sa.sa_handler = &sig_handler;
-    sa.sa_flags = SA_SIGINFO;
-    if (sigaction(SIGUSR1, &sa, NULL) == -1)
-    {
-        ft_printf("ERROR\n");
-        exit(1);
-    }
-    if (sigaction(SIGUSR2, &sa, NULL) == -1)
-    {
-        ft_printf("ERROR\n");
-        exit(1);
-    }
+	if (argc != 3)
+		ft_printf("Invalid number of arguments");
+	if (*argv[2] == 0)
+		ft_printf("Invalid message (empty)");
 }
 
-static void sig_handler(int signum)
+void	send_msg(pid_t sv_pid, char *msg)
 {
-    if (signum == SIGUSR2)
-        ft_printf("caractere recus!\n");
+	unsigned char	c;
+	int				nbr_bits;
+
+	while (*msg)
+	{
+		c = *msg;
+		nbr_bits = 8;
+		while (nbr_bits--)
+		{
+			if (c & 0b10000000)
+				kill(sv_pid, SIGUSR1);
+			else
+				kill(sv_pid, SIGUSR2);
+			usleep(50);
+			c <<= 1;
+		}
+		msg++;
+	}
+	c = *msg;
+	nbr_bits = 8;
+	while (nbr_bits--)
+	{
+		if (c & 0b10000000)
+			kill(sv_pid, SIGUSR1);
+		else
+			kill(sv_pid, SIGUSR2);
+		usleep(50);
+		c <<= 1;
+	}
 }
 
-static void ft_send_msg(pid_t pid_server, char *msg)
+void	sig_handler(int signum)
 {
-    unsigned char    c;
-    int nb_bits;
+	if (signum == SIGUSR2)
+	{
+		ft_printf("message has been sucessfully receieved!\n");
+		exit(0);
+	}
+}
 
-    while (*msg)
-    {
-        c = *msg;
-        nb_bits = 8;
-        while (nb_bits--)
-        {
-            if (c & 0b10000000)
-                kill(pid_server, SIGUSR1);
-            else
-                kill(pid_server, SIGUSR2);
-            usleep(50);
-            c <<= 1;
-        }
-        msg++;
-    }
-    c = *msg;
-    nb_bits = 8;
-    while (nb_bits--)
-    {
-        if (c & 0b10000000)
-            kill(pid_server, SIGUSR1);
-        else
-            kill(pid_server, SIGUSR2);
-        usleep(50);
-        c <<= 1;
-    }
+void	config_signals(void)
+{
+	struct sigaction	sa_newsig;
+
+	sa_newsig.sa_handler = &sig_handler;
+	sa_newsig.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa_newsig.sa_mask);
+	if (sigaction(SIGUSR1, &sa_newsig, NULL) == -1)
+		ft_printf("Failed to change SIGUSR1's behavior");
+	if (sigaction(SIGUSR2, &sa_newsig, NULL) == -1)
+		ft_printf("Failed to change SIGUSR2's behavior");
+}
+
+int	main(int argc, char **argv)
+{
+	pid_t		sv_pid;
+
+	args_check(argc, argv);
+	sv_pid = ft_atoi(argv[1]);
+	config_signals();
+	send_msg(sv_pid, argv[2]);
+	while (1)
+		pause();
+	return (0);
 }
--- server_bonus.c
+++ server_bonus.c
@@ -6,73 +6,127 @@
 /*   By: zramahaz <zramahaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 14:21:01 by zramahaz          #+#    #+#             */
-/*   Updated: 2024/06/29 15:33:35 by zramahaz         ###   ########.fr       */
+/*   Updated: 2024/07/11 14:21:12 by zramahaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void ft_config_signals(void);
-static void sig_handler(int signum, siginfo_t *info, void *content);
-
-int main(void)
+static size_t	ft_strlen(const char *str)
 {
-    pid_t   pid_server;
+	size_t	i;
 
-    pid_server = getpid();
-    ft_printf("PID SERVER = %d\n", pid_server);
-    while (1)
-        ft_config_signals();
-    return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return(i);
 }
 
-static void ft_config_signals(void)
+static char	*ft_strcpy(char *dest, char const *src)
 {
-    struct sigaction  sa;
+	int	i;
 
-    sa.sa_sigaction = &sig_handler;
-    sa.sa_flags = SA_SIGINFO;
-    if (sigaction(SIGUSR1, &sa, NULL) == -1)
-    {
-        ft_printf("ERROR\n");
-        exit(1);
-    }
-    if (sigaction(SIGUSR2, &sa, NULL) == -1)
-    {
-        ft_printf("ERROR\n");
-        exit(1);
-    }
+	i = -1;
+	while (src[++i] != '\0')
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
 }
 
-static void sig_handler(int signum, siginfo_t *info, void *content)
+static char	*ft_strcat(char *dest, char const *src)
 {
-    static int  bit_itr = -1;
-    static unsigned char    c;
+	int	i;
+	int	j;
 
-    (void)content;
-    if (bit_itr < 0)
-        bit_itr = 7;
-    if (signum == SIGUSR1)
-        c |= 1 << bit_itr;
-    bit_itr--;
-    if (bit_itr < 0 && c)
-    {
-        ft_printf("%c", c);
-        c = 0;
-        return ;
-    }
+	i = 0;
+	while (dest[i])
+		i++;
+	j = -1;
+	while (src[++j])
+		dest[i++] = src[j];
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	size_t	len;
+
+	len = ft_strlen(s1);
+	len += ft_strlen(s2);
+	dest = (char *)malloc(len + 1);
+	if (!dest)
+		return (NULL);
+	ft_strcpy(dest, s1);
+	ft_strcat(dest, s2);
+	free((char *)s1);
+	return (dest);
+}
+
+char	*ft_stack_str(char *str, char c)
+{
+	char	*dest;
+
+	if (str == NULL)
+	{
+		str = malloc(sizeof(char));
+		*str = '\0';
+	}
+	dest = ft_strjoin(str, &c);
+	return (dest);
+}
+
+void	handle_sigusr(int signum, siginfo_t *info, void *ucontent)
+{
+	static int				bit_itr = -1;
+	static unsigned char	c;
+	static char				*str;
+	
+	(void)ucontent;
+	if (bit_itr < 0)
+		bit_itr = 7;
+	if (signum == SIGUSR1)
+		c |= (1 << bit_itr);
+	bit_itr--;
+	if (bit_itr < 0 && c)
+	{
+		// ft_printf("%c", c);
+		str = ft_stack_str(str, c);
+		c = 0;
+		return ;
+	}
     if (bit_itr < 0 && c == 0)
+	{
+		ft_printf("str = |%s|\n", str);
+		free(str);
+		str = NULL;
+        kill(info->si_pid, SIGUSR2);
+	}
+}
+
+void	config_signals(void)
+{
+	struct sigaction	sa_newsig;
+
+	sa_newsig.sa_sigaction = &handle_sigusr;
+	sa_newsig.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa_newsig.sa_mask);
+	if (sigaction(SIGUSR1, &sa_newsig, NULL) == -1)
+		ft_printf("Failed to change SIGUSR1's behavior");
+	if (sigaction(SIGUSR2, &sa_newsig, NULL) == -1)
+		ft_printf("Failed to change SIGUSR2's behavior");
+}
+
+int	main(void)
+{
+	pid_t	pid;
+
+	pid = getpid();
+	ft_printf("SERVER PID = %d\n\n", pid);
+	config_signals();
+    while (1)
     {
-        if (kill(info->si_pid, SIGUSR2) == -1)
-        {
-            ft_printf("ERROR\n");
-            exit(1);
-        }    
     }
-    
-    if (kill(info->si_pid, SIGUSR1) == -1)
-    {
-        ft_printf("ERROR\n");
-        exit(1);
-    }
+	return (0);
 }
